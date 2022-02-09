01e4e14eea49ede61cd9750163771e5be28db0df
swapn7038 <82700394+swapn7038@users.noreply.github.com>
2022-02-09 14:43:43 +0530

    Updated

---
+++ CollegeAssignment/arrays.c
@@ -0,0 +1,34 @@
+// #include<stdio.h>
+
+// int main(){
+//     double arr[] = {1.2, 2.3, 4.5, 3.6, 9.5}; 
+
+// // Need to declare "int i"  here 
+//      for(int i=0; i<5; i++){
+//              printf("%.2lf \n ", arr[i]); 
+
+//      }
+
+//     return 0; 
+
+
+// }
+
+
+
+// #include<stdio.h>
+
+// int main(){
+//     int marks[] = {35, 45, 56, 75, 85, 95, 05, 15, 25, 35, 454, 55};
+ 
+//     for(int i=0; i=12; i++){
+//          printf("%d\n ", marks[i]); 
+
+//          return 0; 
+//     }
+// }
+
+//  This one doesnt work find the mistake  here 
+
+
+#in
